9b4c481b494d514b54caef92c9ba098830c40659
ruskofulanito <ruskito@gmail.com>
2017-11-21 21:38:14 -0300

    Update dropbox.php

--- dropbox.php
+++ dropbox.php
@@ -12,7 +12,7 @@
 <body>
 	<section class="container main">
 		<form onSubmit="return uploadFile()">
-			<input type="text" id="numeroSiniestro" placeholder="Numero de siniestro" required="true" />
+			<input type="text" id="numeroSiniestro" placeholder="Numero de siniestro" required="true" enabled="false" value="<?php echo $_GET['id']; ?> />
 			<input type="file" id="file-upload" required="true" accept="image/*"/>
 			<button class="btn waves-effect waves-light" type="submit" name="action">Enviar foto</button>
 		</form>
@@ -23,6 +23,7 @@
 
 	<script>
 		function uploadFile() {
+				results.innerHTML('Subiendo archivo...');
 			var numeroSiniestro = document.getElementById('numeroSiniestro').value;
 			var dbx = new Dropbox({ accessToken: "MY1nVgonrBAAAAAAAAAACzJYPIzn8ALYJH2HIp8-cwh0kjwg2UyMnzhL7CnWsls_" });
 			var fileInput = document.getElementById('file-upload');
@@ -30,7 +31,7 @@
 			dbx.filesUpload({path: '/'+numeroSiniestro+ '/' + file.name, contents: file})
 			.then(function(response) {
 				var results = document.getElementById('results');
-				results.appendChild(document.createTextNode('File uploaded!'));
+				results.innerHTML('El archivo fue guardado con éxito');
 				console.log(response);
 			})
 			.catch(function(error) {
