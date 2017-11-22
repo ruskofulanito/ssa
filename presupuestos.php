31a1d1d99ad5a911479aac66e8179e6c169848dd
ruskofulanito <ruskito@gmail.com>
2017-11-22 14:13:11 -0300

    Update presupuestos.php

--- presupuestos.php
+++ presupuestos.php
@@ -2,18 +2,22 @@
 class Presupuesto{
 	public function getPresupuesto($consulta){
 		$consulta = json_decode($consulta,true);
-		$jsonARetornar = array("objetos"=>array());
+		$jsonARetornar = array("status"=>"ok", "objetos"=>array());
 		//$jsonARetornar['token']= $consulta['token'];
 		$total = 0;
-		foreach ($consulta['objetos'] as $item){
-			$precio = $this->getRandom();
-			$subtotal = number_format($precio*$item['cantidad'], 2, ".", "");
-			$itemARetornar = ["id"=>$item[id], "precioUnitario"=>$precio, "subtotal" => $subtotal];
-			$total += $subtotal
-			array_push($jsonARetornar['objetos'], $itemARetornar);
+		if (is_array($consulta) && is_array($consulta['objetos'])) {
+			foreach ($consulta['objetos'] as $item){
+				$precio = $this->getRandom();
+				$subtotal = number_format($precio*$item['cantidad'], 2, ".", "");
+				$itemARetornar = ["id"=>$item[id], "precioUnitario"=>$precio, "subtotal" => $subtotal];
+				$total += $subtotal;
+				array_push($jsonARetornar['objetos'], $itemARetornar);
+			}
+			$jsonARetornar['total'] = number_format($total,2,".","");;
+			return json_encode($jsonARetornar, JSON_UNESCAPED_UNICODE);
+		} else {
+			return json_encode(["status" => "error"]);
 		}
-		$jsonARetornar['total'] = number_format($total,2,".","");;
-		return json_encode($jsonARetornar, JSON_UNESCAPED_UNICODE);
 	}
 	protected function getRandom($digitos = 5){
 		return number_format((rand(str_pad(1, $digitos+2,"0"),str_pad(9, $digitos+2,"9"))/100),2,".","");
